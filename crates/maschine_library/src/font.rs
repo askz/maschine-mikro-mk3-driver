775e47b2b0354a0682b7695192a01e32ee69ae54
Maxime Saddok <saddok.ma@gmail.com>
2026-01-18 16:53:29 +0000

    feat(font): add letter support and refactor glyph rendering
    - Add complete A-Z alphabet glyphs (26 letters)
    - Refactor digit rendering to use common glyph function
    - Add write_char() and write_str() methods for text rendering
    - Introduce Glyph type alias for better code clarity
    
    This enables displaying arbitrary text on the device screen,
    not just numeric values.

--- crates/maschine_library/src/font.rs
+++ crates/maschine_library/src/font.rs
@@ -1,6 +1,8 @@
 use crate::screen::Screen;
 
-const FONT: [[&[u8; 8]; 8]; 10] = [
+type Glyph = [&'static [u8; 8]; 8];
+
+const DIGITS: [Glyph; 10] = [
     // 0
     [
         b"   xxx  ",
@@ -12,8 +14,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b"  x   x ",
         b"   xxx  ",
     ],
+    // 1
     [
-        // 1
         b"     xx ",
         b"     xx ",
         b"    x x ",
@@ -23,8 +25,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b"      x ",
         b"  xxxxxx",
     ],
+    // 2
     [
-        // 2
         b"   xxxx ",
         b" x     x",
         b" x     x",
@@ -34,8 +36,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b" x      ",
         b" xxxxxxx",
     ],
+    // 3
     [
-        // 3
         b"  xxxxx ",
         b" x     x",
         b"      x ",
@@ -45,8 +47,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b" x    x ",
         b"  xxxx  ",
     ],
+    // 4
     [
-        // 4
         b" x     x",
         b" x     x",
         b" x     x",
@@ -56,8 +58,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b"       x",
         b"       x",
     ],
+    // 5
     [
-        // 5
         b" xxxxxxx",
         b" x      ",
         b" x      ",
@@ -67,8 +69,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b"       x",
         b" xxxxxx ",
     ],
+    // 6
     [
-        // 6
         b"  xxxxx ",
         b" x     x",
         b" x      ",
@@ -78,8 +80,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b" x     x",
         b"  xxxxx ",
     ],
+    // 7
     [
-        // 7
         b" xxxxxxx",
         b"       x",
         b"       x",
@@ -89,8 +91,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b"   x    ",
         b"  x     ",
     ],
+    // 8
     [
-        // 8
         b"  xxxxx ",
         b" x     x",
         b" x     x",
@@ -100,8 +102,8 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
         b" x     x",
         b"  xxxxx ",
     ],
+    // 9
     [
-        // 9
         b"  xxxxx ",
         b" x     x",
         b" x     x",
@@ -113,16 +115,325 @@ const FONT: [[&[u8; 8]; 8]; 10] = [
     ],
 ];
 
+const LETTERS: [Glyph; 26] = [
+    // A
+    [
+        b"   xx   ",
+        b"  x  x  ",
+        b" x    x ",
+        b" x    x ",
+        b" xxxxxx ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // B
+    [
+        b" xxxxx  ",
+        b" x    x ",
+        b" x    x ",
+        b" xxxxx  ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" xxxxx  ",
+    ],
+    // C
+    [
+        b"  xxxx  ",
+        b" x    x ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x    x ",
+        b"  xxxx  ",
+    ],
+    // D
+    [
+        b" xxxx   ",
+        b" x   x  ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x   x  ",
+        b" xxxx   ",
+    ],
+    // E
+    [
+        b" xxxxxx ",
+        b" x      ",
+        b" x      ",
+        b" xxxxx  ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" xxxxxx ",
+    ],
+    // F
+    [
+        b" xxxxxx ",
+        b" x      ",
+        b" x      ",
+        b" xxxxx  ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+    ],
+    // G
+    [
+        b"  xxxx  ",
+        b" x    x ",
+        b" x      ",
+        b" x      ",
+        b" x  xxx ",
+        b" x    x ",
+        b" x    x ",
+        b"  xxxx  ",
+    ],
+    // H
+    [
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" xxxxxx ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // I
+    [
+        b"  xxxxx ",
+        b"    x   ",
+        b"    x   ",
+        b"    x   ",
+        b"    x   ",
+        b"    x   ",
+        b"    x   ",
+        b"  xxxxx ",
+    ],
+    // J
+    [
+        b"   xxxx ",
+        b"     x  ",
+        b"     x  ",
+        b"     x  ",
+        b"     x  ",
+        b" x   x  ",
+        b" x   x  ",
+        b"  xxx   ",
+    ],
+    // K
+    [
+        b" x    x ",
+        b" x   x  ",
+        b" x  x   ",
+        b" xxx    ",
+        b" x  x   ",
+        b" x   x  ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // L
+    [
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" xxxxxx ",
+    ],
+    // M
+    [
+        b" x    x ",
+        b" xx  xx ",
+        b" x xx x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // N
+    [
+        b" x    x ",
+        b" xx   x ",
+        b" x x  x ",
+        b" x  x x ",
+        b" x   xx ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // O
+    [
+        b"  xxxx  ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b"  xxxx  ",
+    ],
+    // P
+    [
+        b" xxxxx  ",
+        b" x    x ",
+        b" x    x ",
+        b" xxxxx  ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+        b" x      ",
+    ],
+    // Q
+    [
+        b"  xxxx  ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x  x x ",
+        b" x   x  ",
+        b"  xxx x ",
+    ],
+    // R
+    [
+        b" xxxxx  ",
+        b" x    x ",
+        b" x    x ",
+        b" xxxxx  ",
+        b" x  x   ",
+        b" x   x  ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // S
+    [
+        b"  xxxx  ",
+        b" x    x ",
+        b" x      ",
+        b"  xxxx  ",
+        b"      x ",
+        b"      x ",
+        b" x    x ",
+        b"  xxxx  ",
+    ],
+    // T
+    [
+        b" xxxxxx ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+    ],
+    // U
+    [
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b"  xxxx  ",
+    ],
+    // V
+    [
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b"  x  x  ",
+        b"  x  x  ",
+        b"   xx   ",
+    ],
+    // W
+    [
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x    x ",
+        b" x xx x ",
+        b" xx  xx ",
+        b" x    x ",
+    ],
+    // X
+    [
+        b" x    x ",
+        b" x    x ",
+        b"  x  x  ",
+        b"   xx   ",
+        b"   xx   ",
+        b"  x  x  ",
+        b" x    x ",
+        b" x    x ",
+    ],
+    // Y
+    [
+        b" x    x ",
+        b" x    x ",
+        b"  x  x  ",
+        b"   xx   ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+        b"   x    ",
+    ],
+    // Z
+    [
+        b" xxxxxx ",
+        b"      x ",
+        b"     x  ",
+        b"    x   ",
+        b"   x    ",
+        b"  x     ",
+        b" x      ",
+        b" xxxxxx ",
+    ],
+];
+
 pub struct Font {}
 
 impl Font {
-    pub fn write_digit(s: &mut Screen, y: usize, x: usize, num: usize, scale: usize) {
-        let sym = FONT[num];
+    fn write_glyph(s: &mut Screen, y: usize, x: usize, glyph: &Glyph, scale: usize) {
         for i in 0..(8 * scale) {
             for j in 0..(8 * scale) {
-                let bit = sym[i / scale][j / scale] != b' ';
+                let bit = glyph[i / scale][j / scale] != b' ';
                 s.set(i + y, j + x, bit);
             }
+        }
+    }
+
+    pub fn write_digit(s: &mut Screen, y: usize, x: usize, num: usize, scale: usize) {
+        Self::write_glyph(s, y, x, &DIGITS[num], scale);
+    }
+
+    pub fn write_char(s: &mut Screen, y: usize, x: usize, ch: char, scale: usize) {
+        let glyph = match ch {
+            '0'..='9' => &DIGITS[(ch as usize) - ('0' as usize)],
+            'A'..='Z' => &LETTERS[(ch as usize) - ('A' as usize)],
+            'a'..='z' => &LETTERS[(ch as usize) - ('a' as usize)],
+            _ => return, // unsupported character, skip
+        };
+        Self::write_glyph(s, y, x, glyph, scale);
+    }
+
+    pub fn write_str(s: &mut Screen, y: usize, x: usize, text: &str, scale: usize) {
+        let char_width = 8 * scale;
+        for (i, ch) in text.chars().enumerate() {
+            Self::write_char(s, y, x + i * char_width, ch, scale);
         }
     }
 }
